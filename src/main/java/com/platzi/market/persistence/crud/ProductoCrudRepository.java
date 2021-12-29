e755bc7b1b000528752d601ab50818cd1a0ccef4
Follegatti <e.follegatti.velarde@gmail.com>
2021-12-28 20:01:02 -0500

    Se agrego la clase ProductoRepository

---
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,17 @@
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends JpaRepository<Producto,Integer> {
+
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+}
